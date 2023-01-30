3e330cc5301361ad17c9fdd33d07677071a39893
rakuun76 <rakuun76@gmail.com>
2023-01-30 18:01:47 +0900

    coin 변환기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState("");
+  const [index, setIndex] = useState(0);
+
+  const onChange = (event) => setValue(event.target.value);
+  const onSelect = (event) => setIndex(event.target.value);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select value={index} onChange={onSelect}>
+          {coins.map((coin, idx) => (
+            <option key={idx} value={idx}>
+              {coin.name} ({coin.symbol}) ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <hr />
+      <div>
+        <label htmlFor="USD">USD </label>
+        <input
+          id="USD"
+          type="number"
+          placeholder="Put USD"
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="COIN">{loading ? null : coins[index].name} </label>
+        <input
+          id="COIN"
+          type="number"
+          value={loading ? "" : value / coins[index].quotes.USD.price}
+          readOnly
+        />
+      </div>
     </div>
   );
 }
-
 export default App;
